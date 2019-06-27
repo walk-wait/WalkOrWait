844183f244797fe1187ea17ea67ce8a14ef5c2a4
Quang <chieunq@gmail.com>
2019-06-27 10:06:00 -0400

    my first commit. When run the app, you should see: 'If you see this message, it means that the the connection with the dabase is OKgit status'

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -27,29 +29,53 @@ app.use(routes);
 // Connect to passport
   //CODE HERE ---
 
+//=== Quang ======
+// Initialize passport and the express and passport session and add them as middleware
+// For passport
 
+app.use(session({secret: "123", resave: true, saveUninitialized: true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+// === / Quang ====
 
 // Connect to Mongoose or Sequelize
   // CODE HERE ---
 
-
+  //=======================================================================================
+  //  I run testing but it doesn't work so just comment the below codes out temporarily 
+  //========================================================================================
 
 // Start the API server
-var syncOptions = { force: false };
+// var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = false;
-}
+// if (process.env.NODE_ENV === "test") {
+  // syncOptions.force = false;
+// }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function () {
-  app.listen(PORT, function () {
-    console.log(
-      "==> 🌎  API Server now listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+// db.sequelize.sync(syncOptions).then(function () {
+  // app.listen(PORT, function () {
+    // console.log(
+      // "==> 🌎  API Server now listening on port %s. Visit http://localhost:%s/ in your browser.",
+      // PORT,
+      // PORT
+    // );
+  // });
+// });
+
+// ==== Try to test :
+
+// Models:
+var models = require("./models");
+
+// Synchronizing Database
+
+models.sequelize.sync()
+.then(function() {
+  console.log("If you see this message, it means that the the connection with the dabase is OK!!")
+}).catch(function(err) {
+  console.log(err, "Woof, something went wrong!")
 });
